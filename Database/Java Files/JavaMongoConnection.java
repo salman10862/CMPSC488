b6b51f815196d415b1f4d77d0645eb09386fa786
pranav412 <pranav726@live.com>
2018-02-26 17:39:29 -0500

    Update JavaMongoConnection.java
    Updated a few of the methods and slightly modified the class

--- Database/Java Files/JavaMongoConnection.java
+++ Database/Java Files/JavaMongoConnection.java
@@ -6,57 +6,77 @@ import java.util.ArrayList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
-//C:\Program Files\MongoDB\Server\3.6\bin
+//"mongodb://localhost:27017" --> Connect to local database
 
 public class JavaMongoConnection {
-	public static void main(String args[]) throws UnknownHostException{
-		//Connecting to MongoDB Server (*Note: must add the external jar file first)
-		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));	
+	
+	//Connecting to MongoDB Server (*Note: must add the external jar file first)
+	private static MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));	
+	
+	//choose which database to connect to
+	private static DB database = mongoClient.getDB("ProfileDB");
+	private static DBCollection collection = database.getCollection("Projects");
+	
+	public static void main(String args[]) throws UnknownHostException{		
 		System.out.println("server connection successfully done");
 		
+		//Add a project to database
+		String[] sharedWith = {"aclark123"};
+		addProjectRecord("Sample Project", "pranav412", sharedWith);
+	}
+	
+	//add a record to the database
+	public static void addUserRecord(String fName, String lName, String uname, String pwd){
 		//choose which database to connect to
-		DB database = mongoClient.getDB("ProfileDB");
-		DBCollection userCollection = database.getCollection("users");
+		database = mongoClient.getDB("ProfileDB");	//needs to be imported
+		collection = database.getCollection("users");
 		System.out.println("Database name: " + database.getName());
 		
 		//insert record into database
 		DBObject newUser = new BasicDBObject();
-		newUser.put("firstName", "James");
-		newUser.put("lastName", "Clark");
-		newUser.put("username", "jclark123");
-		newUser.put("password", "animation123");
+		newUser.put("firstName", fName);
+		newUser.put("lastName", lName);
+		newUser.put("username", uname);
+		newUser.put("password", pwd);
 		
 		//add record to database
-		userCollection.insert(newUser);
-		
-		
+		collection.insert(newUser);
 	}
 	
-	//add a record to the database
-	public static void addRecord(){
-		;
+	public static void addProjectRecord(String projName, String owner, String[] sharedWith){
+		//choose which db to connec to 
+		database = mongoClient.getDB("ProfileDB");
+		collection = database.getCollection("project");
+		
+		//Display the name of the database
+		System.out.println("Database name: " + database.getName());
+		
+		//insert record into database
+		DBObject newProject = new BasicDBObject();
+		newProject.put("projectName", projName);
+		newProject.put("owner", owner);
+		newProject.put("sharedWith", sharedWith);
+		
+		collection.insert(newProject);
 	}
 	
 	//find a record in database
-	public static void findRecordByUsername(){
-		//Connecting to MongoDB Server (*Note: must add the external jar file first)
-		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));	
-		System.out.println("server connection successfully done");
-		
+	public static void findRecordByUsername(String uname){
 		//choose which database to connect to
-		DB database = mongoClient.getDB("ProfileDB");
-		DBCollection userCollection = database.getCollection("users");
+		database = mongoClient.getDB("ProfileDB");
+		collection = database.getCollection("users");
 		System.out.println("Database name: " + database.getName());
 		
 		System.out.println("\n");
 		
 		BasicDBObject whereQuery = new BasicDBObject();
 		whereQuery.put("username", uname);
-		DBCursor cursor = userCollection.find(whereQuery);
+		DBCursor cursor = collection.find(whereQuery);
 		while(cursor.hasNext()){
 			
 			System.out.println(cursor.next());
