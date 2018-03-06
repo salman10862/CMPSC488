2af8dc1a6785f71149b252dcc401e72d2839bbcf
Ian Wright <idwright90@gmail.com>
2018-03-05 22:07:19 -0500

    Add files via upload

---
+++ pennychain/center/APMpyth.java
@@ -0,0 +1,32 @@
+package pennychain.center;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class APMpyth {
+	private File file_loc;
+	
+	
+	public APMpyth(String new_path) {
+		this.file_loc = new File(new_path);
+	}
+	
+	public String sendData() throws IOException {
+		/*
+		 * CALL PYTHON SCRIPT
+		 * 
+		 * AFTER IT RUNS, READ OPTIMIZED RESULTS
+		 */
+		return(this.readData());
+	}
+	
+	private String readData() throws IOException {
+		FileReader f = new FileReader(this.file_loc);
+		
+		f.close();
+		return "";
+	}
+
+}
