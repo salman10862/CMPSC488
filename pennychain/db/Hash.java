dafee1fd5f00a592463b6d663f9c529cad9cbc14
Ian Wright <idwright90@gmail.com>
2018-03-08 04:28:29 -0500

    added salt+hash function

---
+++ pennychain/db/Hash.java
@@ -0,0 +1,45 @@
+package pennychain.db;
+
+import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Hash {
+
+    private static final int SALT_BYTES = 32;
+    private static final int HASH_BYTES = 32;
+    private static final int ITERATIONS = 1000;
+    private static final String PBKDF2_ALG = "PBKDF2WithHmacSHA1";
+
+    /* getHashAndSalt : CharSequence -> String
+     * returns salt and salted+hashed password
+     * as a String in the format salt:saltedHashedPw
+     */
+
+    public String getHashAndSalt(CharSequence password)
+        throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_BYTES];
+        random.nextBytes(salt);
+
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(
+                password.toString().toCharArray(),
+                salt,
+                ITERATIONS,
+                HASH_BYTES * 8); // key length is measured in bits
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALG);
+        byte[] hashedPass = skf.generateSecret(pbeKeySpec).getEncoded();
+
+        String result = DatatypeConverter.printBase64Binary(salt) + ":" +
+                        DatatypeConverter.printBase64Binary(hashedPass);
+
+        return result;
+    }
+}
