7115ac1dbc3ac0c97b30ce8a35f95a74665dd776
jrf5498 <36180060+jrf5498@users.noreply.github.com>
2018-03-16 16:35:19 -0400

    Update OptimizationRequest.java

--- pennychain/center/OptimizationRequest.java
+++ pennychain/center/OptimizationRequest.java
@@ -29,34 +29,34 @@ public class OptimizationRequest {
 	
 	private void bundle_data(String path) throws IOException {
 		FileWriter f = new FileWriter(path);
-		f.write(" Variables" + System.getProperty("line.separator"));
+		f.write("Model" + System.getProperty("line.separator") + "  Variables" + System.getProperty("line.separator"));
 		
 		String obj_func = this.create_objective();
 		
 		while(!(variables.isEmpty())) {
-			f.write(variables.get(0) + System.getProperty("line.separator"));
+			f.write("    int_" + variables.get(0) + " = 0, >=0, <=1" + System.getProperty("line.separator"));
 			variables.remove(0);
 		}
-		f.write(System.getProperty("line.separator") + " Constraints" + System.getProperty("line.separator"));
+		f.write("  End Variables" + System.getProperty("line.separator") + System.getProperty("line.separator") + "  Equations" + System.getProperty("line.separator"));
 		while(!(constraints.isEmpty())) {
-			f.write(constraints.get(0) + System.getProperty("line.separator"));
+			f.write("    " + constraints.get(0) + System.getProperty("line.separator"));
 			constraints.remove(0);
 		}
 		
-		f.write(System.getProperty("line.separator") + " Objective Function "+ System.getProperty("line.separator") + obj_func);
+		f.write(System.getProperty("line.separator") + "    " + obj_func);
 		f.close();
 	}
 	
 	private String create_objective() {
-		String obj_func = "Minimize Z = ";
+		String obj_func = "Minimize ";
 		for(int i = 0; i < variables.size(); i++) {
 			for(int j = 0; j < variables.size(); j++) {
 				if(i != j) {
-					obj_func = obj_func + Math.sqrt((Math.floor((i-j)/varBound))*(Math.floor((i-j)/varBound)) + ((i-j)%varBound)*((i-j)%varBound)) + " * " + variables.get(i) + " + ";
+					obj_func = obj_func + Math.sqrt((Math.floor((i-j)/varBound))*(Math.floor((i-j)/varBound)) + ((i-j)%varBound)*((i-j)%varBound)) + " * " + "int_" + variables.get(i) + " + ";
 				}
 			}
 		}
-		obj_func = obj_func + "0 \n";
+		obj_func = obj_func + "0 " + System.getProperty("line.separator") + "  End Equations" + System.getProperty("line.separator") + "End Model";
 		return obj_func;
 	}
 	
@@ -71,24 +71,29 @@ public class OptimizationRequest {
 		return opmap;
 	}
 	
-	private void read_map() {
-
+	private void read_map(String path) {
+		
+		FileWriter f = new FileWriter(path);
 		for(int i = 0; i < varBound; i++) {
         		for(int j = 0; j < varBound; j++) {
         			if(map.map_data[i][j] == 0) {
-        				variables.add("X[" + (i*varBound+j) + "]");//1, >=0, >=1");
+        				variables.add("X[" + (i*varBound+j) + "]");
         			} else if (map.map_data[i][j] == 1) {
-        				variables.add("X[" + (i*varBound+j) + "]");//1, >=0, >=1");
+        				variables.add("X[" + (i*varBound+j) + "]");
         				constraints.add("X[" + (i*varBound+j) + "]=1");
         			}
         		}
 		}
+		for(int x = 0; x < variables.size(); x++) {
+			f.write("int_" + variables.get(x) + System.getProperty("line.separator"));
+                }
+                f.close();
 		
 		String max_num_centers = "";
 		for(int i = 0; i < variables.size(); i++) {
-			max_num_centers = max_num_centers + variables.get(i) + "+";
+			max_num_centers = max_num_centers + "int_" + variables.get(i) + "+";
 		}
-		constraints.add(max_num_centers + "0=" + max_posi);
+		constraints.add(max_num_centers + "0 =" + max_posi);
 	}
 	
 	public void setMaxCenters(int desired_max) {
