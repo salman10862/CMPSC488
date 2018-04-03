39d3da9f22f9149290ecfb8bce77d9c23cd1fcca
Ian Wright <idwright90@gmail.com>
2018-04-03 01:27:38 -0400

    Fix undefined handler in LoadDbController

--- pennychain/controller/LoadDbController.java
+++ pennychain/controller/LoadDbController.java
@@ -4,12 +4,16 @@ import com.google.gson.Gson;
 import pennychain.usr.UserSession;
 import pennychain.db.Connection_Online;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -31,8 +35,22 @@ public class LoadDbController {
     }
 
     @FXML protected void handleLoad(MouseEvent event) {
-       Window stage = loadButton.getScene().getWindow();
-       Gson gson = new Gson();
-       //TODO finish this
+        Window stage = loadButton.getScene().getWindow();
+        Gson gson = new Gson();
+        //TODO finish this
+    }
+
+    @FXML protected void handleCancel(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StartWindow.fxml"));
+        Stage startWindow = new Stage();
+        StartWindowController controller = new StartWindowController(session);
+        loader.setController(controller);
+        Parent root = loader.load();
+
+        Scene startScene = new Scene(root, 400, 400);
+        startWindow.setTitle("Pennychain - Start or Load a Project");
+        startWindow.setScene(startScene);
+        startWindow.show();
+        ((Node)(event.getSource())).getScene().getWindow().hide();
     }
 }
