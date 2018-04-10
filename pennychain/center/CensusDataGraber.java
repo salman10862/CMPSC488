586f25383e6fcc2bc6de07a33dbd2cf48dcd6403
Chris <chris.williams1227@yahoo.com>
2018-04-09 21:46:03 -0400

    Class to retrieve the data from the Census Reader Class

--- pennychain/center/CensusDataGraber.java
+++ pennychain/center/CensusDataGraber.java
@@ -11,38 +11,55 @@ public class CensusDataGraber {
     public static int itemIndex = 0;
     public static String[] columnContents = new String[68];
 
+    public static void main(String args[]) throws FileNotFoundException {
+        KeywordGenerator keys = new KeywordGenerator();
+        ArrayList<String> all_keys = keys.grabKeywords();
+        System.out.println(all_keys);
+
+    }
+
+
     public static String[] retriveColumnHeader(String[][] data) throws FileNotFoundException {
         int row_index = 0;
+        int column_index = 0;
+        int content_index = 0;
         int counter = 0;
-        int sizeOfArray = data.length;
+        int sizeOfArray = data[0].length;
 
         KeywordGenerator keys = new KeywordGenerator();
         ArrayList<String> all_keys = keys.grabKeywords();
 
-        while(sizeOfArray != 0)
+        boolean succeeded = false;
+
+        for (int i = 0; i < sizeOfArray; i++)
         {
-            if(data[row_index][counter] == all_keys.get(counter))
+            while (column_index != sizeOfArray - 1)
             {
-                columnContents[counter] = data[row_index][counter];
-                break;
+                if (data[row_index][column_index] == all_keys.get(counter))
+                {
+                    columnContents[content_index] = data[row_index][column_index];
+                    succeeded = true;
+                    break;
+                }
+                else
+                {
+                    column_index++;
+                    itemIndex++;
+                    sizeOfArray--;
+                }
             }
-            else
-            {
-                counter++;
-                itemIndex++;
-                sizeOfArray--;
-            }
+            counter++;
         }
 
         return columnContents;
     }
 
-    public static String[] retrieveColumnContents(String[][] data, int row_count)
+    public static String[] retrieveColumnContents(String[][] data)
     {
         int row_index = 1;
         int counter = 1;
 
-        while(row_index != row_count)
+        while(row_index != data.length)
         {
             columnContents[counter] = data[row_index][itemIndex];
             counter++;
