85e119b6d53ec25a04671137abdc6881a84182d2
Jason <jrf5498@psu.edu>
2018-04-21 03:40:25 -0400

    Revert to old python process building method & repair to getMinDistanceSupply throwing null pointer exception

--- pennychain/center/APMpyth.java
+++ pennychain/center/APMpyth.java
@@ -6,25 +6,25 @@ public class APMpyth {
 	private File file_loc;
 	
 	
-	public APMpyth(String new_path) {
-		this.file_loc = new File(new_path);
-	}
+	public APMpyth(String new_path) { this.file_loc = new File(new_path); }
 	
 	public String sendData() throws IOException {
-		ProcessBuilder pb = new ProcessBuilder("pennychain\\center\\Optimization.exe", "pennychain\\center\\Optimization.py");
+		ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Jason\\AppData\\Local\\Programs\\Python\\Python36\\python.exe","pennychain\\center\\Optimization.py");
 		Process p = pb.start();
-		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
 		System.out.println(".....start python.....");
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		StringBuilder sb = new StringBuilder();
 		String line = "";
 		while((line = br.readLine()) != null) {
 			System.out.println("Py:  " + line);
+			sb.append(line);
 		}
-		return(this.readData());
+		return(sb.toString());
 	}
 	
 	private String readData() throws IOException {
 		FileReader f = new FileReader(this.file_loc);
-		
 		f.close();
 		return "";
 	}
