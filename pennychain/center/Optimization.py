084a1676aa6ca41d1955df4cb75da36342ae5b4c
Gfellha <36118564+Gfellha@users.noreply.github.com>
2018-04-22 15:29:26 -0400

    Update Optimization.py

--- pennychain/center/Optimization.py
+++ pennychain/center/Optimization.py
@@ -17,12 +17,7 @@ file2.close()
 print(x)
 file = open("TestFile.txt", "w")
 file.write('--- Results of the Optimization Problem --- \n')
-#y = 0
-#for y in range (0, len(x)):
-#	file.write(x[y] + ': '+str(sol[str(x[y])])+ "\n")
-file.write(x[0] + ': '+str(sol['int_x[0]'])+ "\n")
-file.write(x[1] + ': '+str(sol['int_x[2]'])+ "\n")
-file.write(x[2] + ': '+str(sol['int_x[3]'])+ "\n")
-file.write(x[3] + ': '+str(sol['int_x[10]'])+ "\n")
-file.write(x[4] + ': '+str(sol['int_x[12]'])+ "\n")
+y = 0
+for y in range (0, len(x)):
+	file.write(x[y] + ': '+str(sol[x[y]])+ "\n")
 file.close()
